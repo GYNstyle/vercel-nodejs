f54357abffd946ae061536e05d905971c423aaaa
guoguo <guoguo@mini-m1.local>
2023-05-02 16:34:07 +0800

    Add cors

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+app.use(
+  cors({
+    origin: "https://vercel-react-gyn.vercel.app",
+  })
+);
 app.get("/", (req, res) => {
   res.send("Hello from GYN");
 });
